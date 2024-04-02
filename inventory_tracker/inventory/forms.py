0ae5d47183b4494d463d75ac54773ef74789b07c
Reeba Patel <reebapatel2210@gmail.com>
2024-04-02 20:36:33 +0530

    [final commit]

--- inventory_tracker/inventory/forms.py
+++ inventory_tracker/inventory/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Category,InventoryItem
 
 class AdminRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,5 +9,11 @@ class AdminRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class InventoryItemForm(forms.ModelForm):
+	category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
+	class Meta:
+		model = InventoryItem
+		fields = ['name', 'quantity', 'category'] 
 
 
